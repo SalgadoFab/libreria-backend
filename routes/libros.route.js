abb0c4b8b31a63dc84cc33f382bbf03c34d3a593
Fernando <fernandocorralesm76@gmail.com>
2022-04-21 09:12:36 -0600

    arreglo en routes de libros y medios de pago

--- routes/libros.route.js
+++ routes/libros.route.js
@@ -40,13 +40,40 @@ router.post('/registrar-libro', (request, response) => {
     });
 }); //Ruta para registrar un libro
 
-router.get('/obtener-libros', (request, response) => {}); //Ruta para obtener todos los usuario
+router.get('/obtener-libros', (request, response) => {
+    Libro.find((error, lista) => {
+        if (error) {
+            response.json({
+                msj: 'No se pudo listar libros',
+                error
+            });
+        } else {
+            response.json({
+                lista
+            });
+        }
+    });
+}); //Ruta para obtener todos los libros
 
-router.get('/obtener-libro', (request, response) => {}); //Ruta para obtener un solo usuario
 
-router.put('/actualizar-libro', (request, response) => {}); //Ruta para actualizar usuario
+router.get('/obtener-libro', (request, response) => {
+    Libro.find((error, lista) => {
+        if (error) {
+            response.json({
+                msj: 'No se pudo listar libros',
+                error
+            });
+        } else {
+            response.json({
+                lista
+            });
+        }
+    });
+}); //Ruta para obtener un solo libro
 
-router.delete('/eliminar-libro', (request, response) => {}); //Ruta para eliminar usuario
+router.put('/actualizar-libro', (request, response) => {}); //Ruta para actualizar libro
+
+router.delete('/eliminar-libro', (request, response) => {}); //Ruta para eliminar libro
 
 
 //Middleware
