39914c6edacb7e2bc8f0c8d477b05af6563bbf6a
wang-pixel-2003 <75237417+wang-pixel-2003@users.noreply.github.com>
2022-04-22 10:05:05 -0600

    Parametros5

--- models/parametros.model.js
+++ models/parametros.model.js
@@ -4,22 +4,22 @@
 const mongoose = require('mongoose');
 
 //Creamos el esquema de los datos del parametro
-const CantidadMinimaLibroFan = new mongoose.Schema({
+const schemaCantidadMinimaLibroFan = new mongoose.Schema({
     cantidadActual: { type: Number, required: true },
     nuevaCantidad: { type: Number, required: true },
 });
 
-const PrecioActualPorEnvio = new mongoose.Schema({
+const schemaPrecioActualPorEnvio = new mongoose.Schema({
     precioActual: { type: Number, required: true },
     nuevoPrecio: { type: Number, required: true },
 });
 
-const TarifaImpuestoAplicado = new mongoose.Schema({
+const schemaTarifaImpuestoAplicado = new mongoose.Schema({
     tarifaActual: { type: Number, required: true },
     nuevoTarifa: { type: Number, required: true },
 });
 
-const TarifaDescuentoLibroFan = new mongoose.Schema({
+const schemaTarifaDescuentoLibroFan = new mongoose.Schema({
     descuentoActual: { type: Number, required: true },
     nuevoDescuento: { type: Number, required: true },
 });
@@ -27,7 +27,7 @@ const TarifaDescuentoLibroFan = new mongoose.Schema({
 
 //Conversion del esquema al modelo
 //Parametros recibidos: (Nombre del Modelo ,  Nombre del Esquema  , Coleccion de Datos en DB )
-module.exports = mongoose.model('Parametro', CantidadMinimaLibroFan, 'parametros');
-module.exports = mongoose.model('Parametro', PrecioActualPorEnvio, 'parametros');
-module.exports = mongoose.model('Parametro', TarifaImpuestoAplicado, 'parametros');
-module.exports = mongoose.model('Parametro', TarifaDescuentoLibroFan, 'parametros');
+module.exports = mongoose.model('CantidadMinimaLibroFan', schemaCantidadMinimaLibroFan, 'parametros');
+module.exports = mongoose.model('PrecioActualPorEnvio', schemaPrecioActualPorEnvio, 'parametros');
+module.exports = mongoose.model('TarifaImpuestoAplicado', schemaTarifaImpuestoAplicado, 'parametros');
+module.exports = mongoose.model('TarifaDescuentoLibroFan', schemaTarifaDescuentoLibroFan, 'parametros');
--- models/tipoIdentificacion.model.js
+++ models/tipoIdentificacion.model.js
@@ -6,10 +6,10 @@ const mongoose = require('mongoose');
 //Creamos el esquema de los datos del usuario 
 const tiposIdentificacion = new mongoose.Schema({
     codigoIdentificacion: { type: Number, required: true, unique: true },
-    nombreIdentificacion: { type: String, required: true, unique: true  },
+    nombreIdentificacion: { type: String, required: true, unique: true },
     digitos: { type: Number, required: true }
 });
 
 //Conversion del esquema al modelo
 //Parametros recibidos: (Nombre del Modelo ,  Nombre del Esquema  , Coleccion de Datos en DB )
-module.exports = mongoose.model('Identificacion', tiposIdentificacion, 'identificacion'); 
+module.exports = mongoose.model('Identificacion', tiposIdentificacion, 'identificacion');
